d481d79e575cf6da90c3cdafcddec57b295ed9a7
Satish Ahuja <63805419+Satish-03@users.noreply.github.com>
2021-05-25 19:52:43 +0530

    change in cisco ssh
    getting cmd from file directly

--- mainfile.py
+++ mainfile.py
@@ -10,13 +10,19 @@ from os import path
 import sys
 import time
 import getpass
+import logging
 
 line = printline()
 
+#Start logging
+logging.basicConfig(filename= "test.log", level=logging.DEBUG)
+logger = logging.getLogger("netmiko")
+
+
 # Check if any transfer file given as input
 if len(sys.argv) < 2:
-   print("No file input......Please try again and input a file to copy")
-   sys.exit()
+    print("No file input......Please try again and input a file to copy")
+    sys.exit()
 
 # Check if the transfer exists 
 sourc_file = sys.argv[1]
@@ -24,17 +30,17 @@ if not path.exists(sourc_file):
     print("File '{}' does not exists.    Try again.....".format(sourc_file))
     sys.exit()
 
-#Opening file
+# Opening file
 cmd = []
-with open(sourc_file,'r') as c:
-     cmdlist = c.readlines()
-     for i in cmdlist:
-         cmd.append(i.strip("\n"))
+with open(sourc_file, 'r') as c:
+    cmdlist = c.readlines()
+    for i in cmdlist:
+        cmd.append(i.strip("\n"))
 
 vendor = input("Select the vendor you wish to push the configs on : 1. Cisco  2. Juniper  3. Arista : ")
 venlow = vendor.lower()
 print(line)
-#getting IP list
+# getting IP list
 ip_list = readcsv()
 print(line)
 print("\nPlease, confirm the below config commands you want to push : ")
@@ -44,80 +50,79 @@ for i in cmd:
 gett = input("\nProceed? [n]y: ")
 
 if gett == 'n':
-   sys.exit()
+    sys.exit()
 
 print(line)
 
 print("\nPlease, confirm if you want to push config commands on: ")
 for ip in ip_list:
     print("\n{}".format(ip))
-#Getting confirmation to proceed or abort
+# Getting confirmation to proceed or abort
 gett = input("\nProceed? [n]y: ")
 
 if gett == 'n':
-   sys.exit()
+    sys.exit()
 
-#Prompting for username and password
+cmd_file = sourc_file
+# Prompting for username and password
 print(line)
 username = input('''\nPlease enter your username : ''')
 
 passwd = getpass.getpass('''\nAnd your password : ''')
 
-print("\n"+line)
+print("\n" + line)
 
 if vendor == 1 or venlow == 'cisco':
 
-#Starting time count 
-   start = time.time()
+    # Starting time count
+    start = time.time()
 
-   for ip in ip_list:
-       username = username
-       passwd = passwd
-       cssh_connect(ip, username, passwd, cmd)
-       print(line)
-#Stoping time count
-   stop = time.time()
+    for ip in ip_list:
+        username = username
+        passwd = passwd
+        cssh_connect(ip, username, passwd, cmd_file)
+        print(line)
+    # Stoping time count
+    stop = time.time()
 
-#Getting total time 
-   consume = stop - start
-#Printing the upload time
-   print("Task completed in {} seconds.".format(consume))
-   print(line)
+    # Getting total time
+    consume = stop - start
+    # Printing the upload time
+    print("Task completed in {} seconds.".format(consume))
+    print(line)
 
 elif vendor == 2 or venlow == 'juniper':
-#Starting time count 
-   start = time.time()
+    # Starting time count
+    start = time.time()
 
-   for ip in ip_list:
-       username = username
-       passwd = passwd
-       jssh_connect(ip, username, passwd, cmd)
-       print(line)
-#Stoping time count
-   stop = time.time()
+    for ip in ip_list:
+        username = username
+        passwd = passwd
+        jssh_connect(ip, username, passwd, cmd)
+        print(line)
+    # Stoping time count
+    stop = time.time()
 
-#Getting total time 
-   consume = stop - start
-#Printing the upload time
-   print("Task completed in {} seconds.".format(consume))
-   print(line)
+    # Getting total time
+    consume = stop - start
+    # Printing the upload time
+    print("Task completed in {} seconds.".format(consume))
+    print(line)
 
 else:
-#Starting time count 
-   start = time.time()
+    # Starting time count
+    start = time.time()
 
-   for ip in ip_list:
-       username = username
-       passwd = passwd
-       assh_connect(ip, username, passwd, cmd)
-       print(line)
-#Stoping time count
-   stop = time.time()
+    for ip in ip_list:
+        username = username
+        passwd = passwd
+        assh_connect(ip, username, passwd, cmd_file)
+        print(line)
+    # Stoping time count
+    stop = time.time()
 
-#Getting total time 
-   consume = stop - start
-#Printing the upload time
-   print("Task completed in {} seconds.".format(consume))
-   print(line)
-
-
+    # Getting total time
+    consume = stop - start
+    # Printing the upload time
+    print("Task completed in {} seconds.".format(consume))
+    print(line)
